8ecb912b32624c1973c3a486a0d22cc47fe49298
Camden McKenzie <camden.mckenzie@outlook.com>
2020-03-12 08:53:10 -0400

    Added Project
    Added all project files from local VS

---
+++ xamarinProject1/inClassAppPractice1/Views/LoginPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace inClassAppPractice1.Views
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+
+
+        }
+
+        async void loginButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new ManorPage());
+        }
+    }
+}
---
+++ xamarinProject1/inClassAppPractice1/Views/PageB.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace inClassAppPractice1.Views
+{
+    public partial class PageB : ContentPage
+    {
+        public PageB()
+        {
+            InitializeComponent();
+        }
+
+        void slider_ValueChanged(System.Object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+            double value = e.NewValue;
+            textLabel.Rotation = value;
+
+            
+        }
+
+        void slider_DragStarted(System.Object sender, System.EventArgs e)
+        {
+            Color color = box.BackgroundColor;
+            color = color.Equals(Color.DodgerBlue) ? Color.Firebrick : Color.DodgerBlue;
+
+            box.BackgroundColor = color;
+        }
+    }
+}
